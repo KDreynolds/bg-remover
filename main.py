81a26ea48c85ab4523fa98606d93f7b03f430811
Kyle Reynolds <kylereynoldsdev@gmail.com>
2023-07-27 15:53:49 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,62 +1,75 @@
+from flask import Flask, request, send_file, logging, Response
+from flask_cors import CORS
+import io
 import os
 import cv2
 import numpy as np
 from PIL import Image as Img, Image
 from torchvision import transforms as T
 import torch
-from flask import Flask, request, send_file
-import io
+import base64
 
-from U_2_Net import U2NET
+from U2Net.model import U2NET
+
+app = Flask(__name__)
+CORS(app)
 
 def remove_background(image):
-    model_dir = os.path.join(os.getcwd(), 'U-2-Net/saved_models/u2net/u2net.pth')
-    net = U2NET(3,1)
-    net.load_state_dict(torch.load(model_dir))
-    if torch.cuda.is_available():
-        net.cuda()
+    app.logger.info(f'Processing image of shape {image.shape} and type {image.dtype}')
+    
+    model_dir = os.path.join(os.getcwd(), 'U2Net/saved_models/u2net/u2net.pth')
+    net = U2NET(3, 1)
+    net.load_state_dict(torch.load(model_dir, map_location=torch.device('cpu')))
     net.eval()
 
     img = Img.fromarray(image).convert('RGB')
     ow, oh = img.size
     transform = T.Compose([
-        T.Resize((320,320)),
+        T.Resize((320, 320)),
         T.ToTensor(),
         T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
     img = transform(img)
     img = img.unsqueeze(0)
 
-    if torch.cuda.is_available():
-        img = img.cuda()
-
     d1, d2, d3, d4, d5, d6, d7 = net(img)
 
-    pred = d1[:,0,:,:]
+    pred = d1[:, 0, :, :]
     pred = torch.sigmoid(pred)
     pred = pred.data.cpu().numpy().squeeze()
     pred = (pred - pred.min()) / (pred.max() - pred.min())
 
     pred = cv2.resize(pred, (ow, oh))
 
-    thresh = pred.copy()
-    thresh[thresh >= 0.5] = 255
-    thresh[thresh < 0.5] = 0
-    thresh = thresh.astype(np.uint8)
+    thresh = post_process_mask(pred)
 
-    orig = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    orig = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     foreground = cv2.bitwise_and(orig, orig, mask=thresh)
 
-    alpha = cv2.cvtColor(thresh, cv2.COLOR_BGR2GRAY)
+    alpha = thresh  # directly use thresh as alpha
 
-    b, g, r = cv2.split(foreground)
-    rgba = [b, g, r, alpha]
-    result = cv2.merge(rgba, 4)
+    r, g, b = cv2.split(foreground)
+    rgba = [r, g, b, alpha]
+    result = cv2.merge(rgba)  # merge to 4-channel image without specifying 4
+
+    app.logger.info(f'Created result image of shape {result.shape} and type {result.dtype}')
 
     return result
 
-app = Flask(__name__)
+def post_process_mask(mask):
+    # Convert the mask to uint8
+    mask = (mask * 255).astype(np.uint8)
+
+    # Threshold the mask
+    _, mask = cv2.threshold(mask, 127, 255, cv2.THRESH_BINARY)
+
+    # Apply morphological operations
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+    mask = cv2.dilate(mask, kernel, iterations = 1)
+    mask = cv2.erode(mask, kernel, iterations = 1)
+
+    return mask
 
 @app.route('/remove_background', methods=['POST'])
 def remove_background_api():
@@ -66,19 +79,18 @@ def remove_background_api():
     result = remove_background(img_array)  # call the remove_background function
 
     # Convert the result to a PIL Image
-    result_img = Image.fromarray(result)
+    result_img = Image.fromarray(cv2.cvtColor(result, cv2.COLOR_BGRA2RGBA))
 
     # Save the result to a BytesIO object
     img_bytes = io.BytesIO()
     result_img.save(img_bytes, format='PNG')
-    img_bytes = img_bytes.getvalue()
+    img_bytes.seek(0)
 
-    # Return the result as a file
-    return send_file(
-        io.BytesIO(img_bytes),
-        mimetype='image/png',
-        as_attachment=True,
-        attachment_filename='output.png')
+    # Create a data URL from the bytes
+    data_url = "data:image/png;base64," + base64.b64encode(img_bytes.getvalue()).decode('utf-8')
+
+    # Return an HTML string with the img tag
+    return f'<img src="{data_url}" alt="Processed Image">'
 
 if __name__ == '__main__':
     app.run(debug=True)
